f0be4486822ae3c6e37877c22b734ff85d75b44e
ts.igov <ts.igov@outlook.com>
2021-11-21 23:08:34 +0200

    Implemented the event store context.

---
+++ Byteology.EventSourcing.EntityFramework/EventStoreContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Byteology.EventSourcing.EntityFramework;
+
+using Byteology.EventSourcing.EventHandling;
+using Byteology.EventSourcing.Storage;
+using Microsoft.EntityFrameworkCore;
+
+public abstract class EventStoreContext<TEventContext> : DbContext, IEventStoreContext
+    where TEventContext : class, IEventContext
+{
+    protected EventStoreContext(DbContextOptions options) : base(options) { }
+
+    protected abstract TEventContext CreateNewRecord(IEventContext eventContext);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TEventContext>();
+    }
+
+    IEnumerable<IEventContext> IEventStoreContext.GetEventStream(Guid aggregateRootId)
+        => Set<TEventContext>().Where(e => e.AggregateId == aggregateRootId).ToList();
+
+    void IEventStoreContext.AddEvents(IEnumerable<IEventContext> eventStream)
+    {
+        Set<TEventContext>().AddRange(eventStream.Select(e => CreateNewRecord(e)));
+        SaveChanges();
+    }
+}
