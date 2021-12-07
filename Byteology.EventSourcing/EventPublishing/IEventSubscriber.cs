fd142b7e0485b537f7c94e009466a5d132acceb5
ts.igov <ts.igov@outlook.com>
2021-12-07 21:49:49 +0200

    Added generic IEventSubscriber interface.

--- Byteology.EventSourcing/EventPublishing/IEventSubscriber.cs
+++ Byteology.EventSourcing/EventPublishing/IEventSubscriber.cs
@@ -4,3 +4,24 @@ public interface IEventSubscriber
 {
     Task OnEventAsync(IEvent @event, EventMetadata metadata);
 }
+
+public interface IEventSubscriber<in TEvent> : IEventSubscriber
+    where TEvent : IEvent
+{
+    Task OnEventAsync(TEvent @event, EventMetadata metadata);
+
+    async Task IEventSubscriber.OnEventAsync(IEvent @event, EventMetadata metadata)
+    {
+        Type eventType = @event.GetType();
+        Type genericSubscriberType = typeof(IEventSubscriber<>).MakeGenericType(eventType);
+
+        if (this.GetType().IsAssignableTo(genericSubscriberType))
+        {
+            Task task = 
+                (genericSubscriberType.GetMethod(nameof(OnEventAsync))!.Invoke(this, new object[] { @event, metadata }) as Task)!;
+
+            await task.ConfigureAwait(false);
+        }
+
+    }
+}
