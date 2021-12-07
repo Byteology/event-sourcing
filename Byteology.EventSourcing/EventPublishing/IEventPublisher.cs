104eae817e8fa864d5312ef7131578c0a9704d7f
ts.igov <ts.igov@outlook.com>
2021-12-07 21:41:20 +0200

    Added interface methods for registering event subscribers.

--- Byteology.EventSourcing/EventPublishing/IEventPublisher.cs
+++ Byteology.EventSourcing/EventPublishing/IEventPublisher.cs
@@ -5,4 +5,7 @@ using Byteology.EventSourcing.EventStorage;
 public interface IEventPublisher
 {
     void PublishEvents(IEnumerable<EventRecord> events);
+
+    void RegisterSynchronousEventSubsriber(IEventSubscriber subscriber);
+    void RegisterAsynchronousEventSubsriber(IEventSubscriber subscriber);
 }
