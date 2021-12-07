670407eaecbe9ee997f0d36e4a301f8426324a24
ts.igov <ts.igov@outlook.com>
2021-12-07 20:48:40 +0200

    Added IEventPublisher.

---
+++ Byteology.EventSourcing/EventPublishing/IEventPublisher.cs
@@ -0,0 +1,8 @@
+﻿namespace Byteology.EventSourcing.EventPublishing;
+
+using Byteology.EventSourcing.EventStorage;
+
+public interface IEventPublisher
+{
+    void PublishEvents(IEnumerable<EventRecord> events);
+}
